b993d678ed2f7b5ca09f0c86d5c1fb4ba5004da7
JB <jack.bush@me.com>
2026-03-25 00:09:10 +0100

    looking better feeling better

--- js/script.js
+++ js/script.js
@@ -5,14 +5,19 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x111118)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 5
 
-const light = new THREE.AmbientLight(0xbadce6)
-scene.add(light)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+scene.add(ambientLight)
 
-const cubeColors = [0xe63f52, 0xf36279, 0xfc92a4, 0xe74c39, 0xff6d37, 0xfbe446, 0xff5200, 0x00ae8e, 0xff4814, 0xf4436d, 0xcd3363, 0x773dbe, 0x8d9ebd, 0x69a3b9, 0x003595, 0x0047bb, 0x54c9e8, 0x30d9c4, 0x75e1c2, 0x71d54c, 0xe3e836]
+const dirLight = new THREE.DirectionalLight(0xffffff, 1.8)
+dirLight.position.set(5, 10, 7)
+scene.add(dirLight)
+
+const cubeColors = [0xf8909c, 0xfba4b0, 0xfebdc8, 0xf8988b, 0xffaa86, 0xfeec85, 0xff9e70, 0x6fcfbf, 0xff967c, 0xfc8faa, 0xe685a4, 0xad8ed6, 0xb1bed6, 0x9ec3d4, 0x7091ca, 0x7699db, 0x9adcf0, 0x84e4d8, 0xa1e9d7, 0xa3e28c, 0xe9eb7f]
 const numberOfShapes = 200
 
 function bricks() {
@@ -20,7 +25,7 @@ function bricks() {
 
   for (let n = 0; n < numberOfShapes; n++) {
     const geometry = new THREE.BoxGeometry(0.5 * Math.random(), 0.5 * Math.random(), 0.5 * Math.random())
-    const material = new THREE.MeshBasicMaterial({ color: cubeColors[Math.floor(Math.random() * cubeColors.length)] })
+    const material = new THREE.MeshLambertMaterial({ color: cubeColors[Math.floor(Math.random() * cubeColors.length)] })
     shapes[n] = new THREE.Mesh(geometry, material)
 
     shapes[n].castShadow = true
@@ -28,6 +33,7 @@ function bricks() {
     shapes[n].translateX(7 * (Math.random() - 0.5))
     shapes[n].translateZ(7 * (Math.random() - 0.5))
     shapes[n].translateY(20 + 7 * (Math.random() - 0.5))
+    shapes[n].drift = 0.02 * (Math.random() - 0.5)
 
     scene.add(shapes[n])
   }
@@ -40,11 +46,12 @@ function bricks() {
       shapes[n].rotation.y += 0.02 * Math.random() + 0.01 * n
       shapes[n].rotation.z += 0.01 * Math.random() + 0.01 * n
       shapes[n].position.y -= 0.04 * Math.random() + 0.01 * n
-      shapes[n].position.x -= 0.02 * (Math.random() - 0.5)
+      shapes[n].position.x += shapes[n].drift
     }
   }
 
   render()
 }
 
-document.body.addEventListener('click', bricks)
+bricks()
+document.querySelector('.trigger').addEventListener('click', bricks)
